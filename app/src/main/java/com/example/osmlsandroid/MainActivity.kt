546ba9f536b6df26f459e5c8e6905eba0996c9e8
Alena <attapi343@gmail.ru>
2021-01-08 15:25:09 +0300

    Added xml components and events of the MainActivity class.

--- app/src/main/java/com/example/osmlsandroid/MainActivity.kt
+++ app/src/main/java/com/example/osmlsandroid/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.osmlsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        var basePath = "http://";
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val saveData = findViewById<View>(R.id.button) as Button
+        saveData.setOnClickListener {
+            val ipserver = findViewById<View>(R.id.editTextTextPersonName) as EditText
+            basePath += ipserver.text.toString()
+           // startActivity(intentMain)
+        }
     }
 }
